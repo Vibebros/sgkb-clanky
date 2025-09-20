34dcda804bc4daff6980a01b6e6c05c62b2b906b
Kenin Kujovic <kkujovic@confirm.ch>
2025-09-20 21:45:08 +0200

    fix django cors

--- sgkb/sgkb/settings.py
+++ sgkb/sgkb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'finance.apps.FinanceConfig',
     'graphene_django',
     'ai_manager.apps.AiManagerConfig'
@@ -47,6 +48,7 @@ GRAPHENE = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -152,3 +154,13 @@ LOGO_API_KEY = os.environ.get("LOGO_DEV_API_KEY")
 
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_ALL_ORIGINS = True  # For development only
