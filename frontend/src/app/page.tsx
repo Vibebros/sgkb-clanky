84426f58284194484ce8d5006b2eaf94d67a8ecb
Kenin Kujovic <kkujovic@confirm.ch>
2025-09-20 21:56:46 +0200

    add initial pie

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import StatsCard from "@/components/ui/stats-card";
+import PieChart from "@/components/ui/pie-chart";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
 
       <div className="text-2xl font-bold text-gray-900 text-left w-full pl-12 pb-10">Welcome back, John!</div>
      <StatsCard />
+     <PieChart />
 
       Pie diagram
 
